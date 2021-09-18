3c7f7f30076b6bfa8f3cca71f5f7aa0d02c9e873
Yunyi Gao <gao622@purdue.edu>
2021-09-18 11:20:16 -0400

    Basic Window created

--- src/LiveMap.java
+++ src/LiveMap.java
@@ -1,5 +1,34 @@
+import javax.swing.*;
+
+/**
+ *
+ *
+ */
+
 public class LiveMap {
+
+    private static void createWindow() {
+        //basic appearance
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        //build and set window
+        JFrame frame = new JFrame("LiveMap Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //
+        JLabel label = new JLabel("Hi");
+        frame.getContentPane().add(label);
+        //display window
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createWindow();
+            }
+        });
+
     }
 }

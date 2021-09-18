810ed6138f03383a7e82057b12cabe0c35fa076c
AlexHunton2 <alexhunton02@gmail.com>
2021-09-18 11:24:59 -0400

    Update Size

--- src/LiveMap.java
+++ src/LiveMap.java
@@ -6,6 +6,8 @@ import javax.swing.*;
  */
 
 public class LiveMap {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
 
     private static void createWindow() {
         //basic appearance
@@ -13,12 +15,13 @@ public class LiveMap {
         //build and set window
         JFrame frame = new JFrame("LiveMap Window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //
         JLabel label = new JLabel("Hi");
         frame.getContentPane().add(label);
         //display window
         frame.pack();
         frame.setVisible(true);
+        //set size
+        frame.setSize(WIDTH, HEIGHT);
     }
 
     public static void main(String args[]) {

9461f16b36627f5a0815ee55e5670a324035cda2
Yunyi Gao <gao622@purdue.edu>
2021-09-18 14:45:58 -0400

    Basic Window created

---
+++ src/LizTestLiveMap.java
@@ -0,0 +1,66 @@
+import Subcomponents.MapContainer.MapContainer;
+import Subcomponents.UIContainer.UIContainer;
+import Subcomponents.RequestContainer.RequestContainer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ *
+ */
+
+public class LizTestLiveMap {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private static void createWindow() {
+        //basic appearance
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        //build and set window
+        JFrame frame = new JFrame("LiveMap Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //display window
+        frame.pack();
+        frame.setVisible(true);
+        //set size
+        frame.setSize(WIDTH, HEIGHT);
+        //set grid layout
+        frame.setLayout(new GridLayout(1, 1));
+
+        MapContainer mapContainer = new MapContainer();
+        UIContainer uiContainer = new UIContainer();
+        JButton button = new JButton("Submit Request");
+
+        //setting button size
+        Dimension size = button.getPreferredSize();
+        button.setBounds(0, 1000, 100, 30);
+        //setting button action
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RequestContainer.openRequest();
+            }
+        }
+        );
+
+
+        frame.add(button);
+        frame.getContentPane().add(uiContainer);
+        frame.getContentPane().add(mapContainer);
+
+    }
+
+    public static void main(String args[]) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createWindow();
+            }
+        });
+
+    }
+}

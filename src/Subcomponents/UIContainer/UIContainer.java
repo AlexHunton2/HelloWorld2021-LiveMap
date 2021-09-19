68ad0114a08bb34af17cfbe967797a0dd7cdf71c
Yunyi Gao <gao622@purdue.edu>
2021-09-19 01:47:44 -0400

    relative path update

--- src/Subcomponents/UIContainer/UIContainer.java
+++ src/Subcomponents/UIContainer/UIContainer.java
@@ -2,19 +2,21 @@ package Subcomponents.UIContainer;
 
 import javax.swing.*;
 
-
 public class UIContainer  extends JPanel {
     public UIContainer() {
         super();
-        JLabel label = new JLabel("Event categories");
-        Icon YellowIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/club.jpg");
+        Icon TitleIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/TitleIcon.jpg");
+        JLabel label = new JLabel(TitleIcon);
+        Icon YellowIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/ClubIcon.jpg");
         JButton button1 = new JButton(YellowIcon);
-        Icon OrangeIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/career.jpg");
+        Icon OrangeIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/CareerIcon.jpg");
         JButton button2 = new JButton(OrangeIcon);
-        Icon RedIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/party.jpg");
+        Icon RedIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/PartyIcon.jpg");
         JButton button3 = new JButton(RedIcon);
 
         Box box = Box.createVerticalBox();
+        box.add(label);
+        box.add(Box.createVerticalStrut(10));
         box.add(button1);
         box.add(Box.createVerticalStrut(10));
         box.add(button2);
@@ -22,7 +24,6 @@ public class UIContainer  extends JPanel {
         box.add(button3);
         box.add(Box.createVerticalStrut(10));
         add(box);
-        add(label);
 
     }
 

9592ef1311bf17a370885ede4288b3fd8c9a44c9
Zyeinyep <zkhaval@purdue.edu>
2021-09-19 00:36:30 -0400

    Sync!

--- src/Subcomponents/UIContainer/UIContainer.java
+++ src/Subcomponents/UIContainer/UIContainer.java
@@ -2,17 +2,27 @@ package Subcomponents.UIContainer;
 
 import javax.swing.*;
 
+
 public class UIContainer  extends JPanel {
     public UIContainer() {
         super();
-        JLabel uiText = new JLabel("UI!");
-        JButton button1 = new JButton("Club");
-        JButton button2 = new JButton("Career");
-        JButton button3 = new JButton("Party");
-        this.add(uiText);
-        this.add(button1);
-        this.add(button2);
-        this.add(button3);
+        JLabel label = new JLabel("Event categories");
+        Icon YellowIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/club.png");
+        JButton button1 = new JButton(YellowIcon);
+        Icon OrangeIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/career.png");
+        JButton button2 = new JButton(OrangeIcon);
+        Icon RedIcon = new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/UIContainer/party.png");
+        JButton button3 = new JButton(RedIcon);
+
+        Box box = Box.createVerticalBox();
+        box.add(button1);
+        box.add(Box.createVerticalStrut(10));
+        box.add(button2);
+        box.add(Box.createVerticalStrut(10));
+        box.add(button3);
+        box.add(Box.createVerticalStrut(10));
+        add(box);
+        add(label);
 
     }
 

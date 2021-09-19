aa1d0b2e7cbe0b58b27036a820c642891aab16e0
Yunyi Gao <gao622@purdue.edu>
2021-09-19 02:57:02 -0400

    relative path update

--- src/Subcomponents/MapContainer/MapContainer.java
+++ src/Subcomponents/MapContainer/MapContainer.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 public class MapContainer extends JPanel {
     public MapContainer() {
         super();
-
         JLabel mapImage1 = new JLabel(new ImageIcon("../HelloWorld2021-LiveMap/src/Subcomponents/MapContainer/purduemap.jpg"));
         this.add(mapImage1);
     }
